aa6c3fdb67a84eae741e5a37f59299dd18727142
Pelmemant <basil.111@yandex.ru>
2024-10-18 16:51:06 +0700

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -1,12 +1,10 @@
 def get_multiplied_digits(number):
-    sum = 1
-    last = number % 10
-    if last == 0:
-        last = 1
-    number = number // 10
-    if number != 0:
-        sum = sum * get_multiplied_digits(number)
-    return sum * last
+    str_number = str(number)
+    first = int(str_number[0])
+    other_digits = str_number[1:] if len(str_number) > 1 else ''
+    result = 1 if not other_digits else get_multiplied_digits(int(other_digits))
+    return first * result
 
-result = get_multiplied_digits(40203)
+
+result = get_multiplied_digits(40302)
 print(result)
