5aa6c7b7650e3d7970b987342704dd745aa4d18a
Pelmemant <basil.111@yandex.ru>
2024-10-27 18:36:56 +0700

    Update module_11_1.py

--- module_11_1.py
+++ module_11_1.py
@@ -1,7 +1,30 @@
 import requests
 
-url = "https://www.royalroad.com/home"
 
-response = requests.get(url)
+def get_html_content(url):
+    response = requests.get(url)
+    return response.text
 
-print(response.text)
+
+def post_form_data(url, data):
+    response = requests.post(url, data=data)
+    return response.json()
+
+
+def check_status_code(url):
+    response = requests.head(url)
+    return response.status_code
+
+
+n = "https://github.com/"
+html_content = get_html_content(n)
+print(html_content[:200])
+form_data = {
+    "username": "user",
+    "password": "password"
+}
+response_json = post_form_data(n, form_data)
+print(response_json)
+
+status_code = check_status_code(n)
+print(status_code)
