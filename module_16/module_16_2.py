debdb427a166ec2d636fc6f00026dd30e1b84760
Pelmemant <basil.111@yandex.ru>
2024-11-03 13:30:58 +0700

    Update module_16_2.py

--- module_16/module_16_2.py
+++ module_16/module_16_2.py
@@ -20,7 +20,7 @@ async def t_def(user_id: int = Path(ge=1, le=100, description='Enter User ID', e
     return {'massage': f"Вы вошли как {user_id}"}
 
 
-@app.get("/user/{username}/{age}'")
+@app.get("/user/{username}/{age}")
 async def fo_def(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
                  age: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='1')]) -> dict:
     return {'massage': f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
