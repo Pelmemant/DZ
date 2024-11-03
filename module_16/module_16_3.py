0d16c1151fdb0d7c3e0d02aeef3987b13debc90a
Pelmemant <basil.111@yandex.ru>
2024-11-03 13:30:43 +0700

    Update module_16_3.py

--- module_16/module_16_3.py
+++ module_16/module_16_3.py
@@ -10,7 +10,7 @@ users = {'1': 'Имя: Example, возраст: 18'}
 async def u_get() -> dict:
     return users
 
-@app.post("/user/{username}/{age}'")
+@app.post("/user/{username}/{age}")
 async def u_post(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
                  age: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='1')]) -> str:
     new_in = str(int(max(users, key=int))+1)
