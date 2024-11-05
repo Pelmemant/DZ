d4b8d360c3a7460aa06f7355bc7ee9a67d0474d4
Pelmemant <basil.111@yandex.ru>
2024-11-05 19:58:01 +0700

    Update module_16_4.py

--- module_16/module_16_4.py
+++ module_16/module_16_4.py
@@ -16,27 +16,36 @@ class User(BaseModel):
 async def u_get() -> List[User]:
     return users
 
-@app.post("/user/{username}/{age}")
+
+@app.post("/user/{username}/{age}'")
 async def u_post(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
-                 age: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='1')]) -> str:
-    new_user = User(id = len(users) + 1, name = username, age =  age)
+                 age: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='1')]) -> User:
+    new_user = User(id = users[-1][0] + 1, name = username, age =  age)
     users.append(new_user)
-    return "Юзер создан"
+    return new_user
+
 
 @app.put("/user/{user_id}/{username}/{age}")
 async def u_put(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='1')], username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
-                 age: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='1')]) -> str:
+                 age: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='1')]) -> User:
     try:
-        users[user_id] = f"Имя {username}, возраст {age}"
-        return "Юзер обновлён"
+        for i in range(len(users)):
+            if user_id == users[i][0]:
+                upd_user = User(id = user_id, name = username, age =  age)
+                users[i] = upd_user
+                return upd_user
+
     except IndexError:
         raise HTTPException(status_code=404, detail='User not found')
 
 
 @app.delete("/user/{user_id}")
-async def u_del(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='1')]) -> str:
+async def u_del(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='1')]) -> User:
     try:
-        users.pop(user_id)
-        return "Юзер удалён"
+        for i in range(len(users)):
+            if user_id == users[i][0]:
+                del_u = users.pop(i)
+                return del_u
+
     except IndexError:
         raise HTTPException(status_code=404, detail='User not found')
