85c6b6853d8dc5cdb3f0496ec1c1e5564b07aa9d
Pelmemant <basil.111@yandex.ru>
2024-11-05 18:26:25 +0700

    Update module_16_3.py

--- module_16/module_16_3.py
+++ module_16/module_16_3.py
@@ -15,15 +15,15 @@ async def u_post(username: Annotated[str, Path(min_length=5, max_length=20, desc
                  age: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='1')]) -> str:
     new_in = str(int(max(users, key=int))+1)
     users[new_in] = f"Имя {username}, возраст {age}"
-    return "Юзер создан"
+    return f"User {new_in} is registered"
 
 @app.put("/user/{user_id}/{username}/{age}")
 async def u_put(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='1')], username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
                  age: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='1')]) -> str:
     users[user_id] = f"Имя {username}, возраст {age}"
-    return "Юзер обновлён"
+    return f"The user {user_id} is registered"
 
 @app.delete("/user/{user_id}")
-async def u_del(user_id: Annotated[str, Path(ge=1, le=100, description='Enter User ID', example='1')]) -> str:
+async def u_del(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='1')]) -> str:
     users.pop(user_id)
-    return "Юзер удалён"
+    return f"The user {user_id} is deleted"
