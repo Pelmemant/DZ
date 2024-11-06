cc75f1cc64e112f0cfaf8636b414e9b0e85fc899
Pelmemant <basil.111@yandex.ru>
2024-11-06 20:57:21 +0700

    Update task.py

--- module_17/module_17_2/app/models/task.py
+++ module_17/module_17_2/app/models/task.py
@@ -1,16 +1,17 @@
 from sqlalchemy import  Column, Integer, String, ForeignKey, Boolean
-from модуль17.module_17_2.app.backend.db import Base
+from module17.module_17_2.app.backend.db import Base
 from sqlalchemy.orm import relationship
-from user import User
+from module17.module_17_3.app.models import *
 
 
 class Task(Base):
     __tablename__ = 'tasks'
+    __table_args__ = {'extend_existing': True}
     id = Column(Integer, primary_key=True)
     title = Column(String)
     content = Column(String)
     priority = Column(Integer, default=0)
     completed = Column(Boolean, default=False)
-    user_id = Column(ForeignKey('users.id'), Integer, index=True ,nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), index=True ,nullable=False)
     slug = Column(String, unique=True, index=True)
     user = relationship("User", back_populates='tasks')
