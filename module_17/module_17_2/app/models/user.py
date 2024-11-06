d4f872ef6b881abf1c4ff48473f7d0ae192e0503
Pelmemant <basil.111@yandex.ru>
2024-11-06 20:57:50 +0700

    Update user.py

--- module_17/module_17_2/app/models/user.py
+++ module_17/module_17_2/app/models/user.py
@@ -1,10 +1,11 @@
 from sqlalchemy import  Column, Integer, String, ForeignKey, Boolean
-from модуль17.module_17_2.app.backend.db import Base
+from module17.module_17_2.app.backend.db import Base
 from sqlalchemy.orm import relationship
-from task import Task
+from module17.module_17_3.app.models import *
 
 class User(Base):
     __tablename__ = 'users'
+    __table_args__ = {'extend_existing': True}
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String)
     firstname = Column(String)
