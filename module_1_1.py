ea33e754287dc30d915e84d4e54c59786804cdde
Pelmemant <basil.111@yandex.ru>
2024-11-14 20:20:09 +0700

    Add files via upload

---
+++ module_1_1.py
@@ -0,0 +1,25 @@
+#1st program
+a = 9**0.5*5
+print(int(a))
+
+#2nd program
+if 9.99 > 9.98 and 1000 != 1000.1:
+    print(True)
+
+#3rd program
+a = 2*2+2
+b = 2*(2+2)
+print(a, b)
+if a != b:
+    print(False)
+
+#4th program
+a = '123.456'
+b = a[4]
+print(b)
+
+#Другое решение
+c = float(a)
+c = int(c*10)
+c = c%10
+print(c)
